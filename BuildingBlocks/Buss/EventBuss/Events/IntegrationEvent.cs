1399af5db8e25b6f8d9f51583128dcf968388fb4
handoreyn <fatihgencaslan@yahoo.com>
2020-11-30 00:30:25 +0300

    evet bus creating.

---
+++ BuildingBlocks/Buss/EventBuss/Events/IntegrationEvent.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Text.Json.Serialization;
+
+namespace ShopDog.BuildingBlocks.Buss.EventBuss.EventBuss
+{
+    public class IntegrationEvent
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreationDate { get; set; }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+    }
+}
